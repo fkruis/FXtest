e8d4170d1e2aee2f9180d0d78e7ad58e68af3c4f
Frits Kruis <fkruis@yahoo.com>
2013-08-16 19:13:44 -0500

    initial

---
+++ src/fremig/fxtest/FXtest.java
@@ -0,0 +1,16 @@
+package fremig.fxtest;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXtest extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
